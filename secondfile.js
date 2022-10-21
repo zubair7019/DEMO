ff950feaa3415e5dd6076a3bec09b573347343fd
mohd zubair <mdzubair3782@gmail.com>
2022-10-21 16:12:28 +0530

    hi

--- secondfile.js
+++ secondfile.js
@@ -1,1 +1,22 @@
-console.log("this is demo")
+// console.log("this is demo")
+// console.dir(document)
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// document.title=123;
+
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+
+///get element by id///
+// console.log(document.getElementById('header-title'))
+// var headerTitle=document.getElementById('header-title')
+// console.log(headerTitle)
+var header=document.getElementById('main-header')
+header.style.borderBottom='solid 3px #000'
+
+//  var additem=document.getElementsByClassName('title')
+//  additem.style.borderBottom=' solid 2px green'
+
