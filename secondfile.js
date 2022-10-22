aef6f3361ddbffb912238d81f8fd060d3a771a79
mohd zubair <mdzubair3782@gmail.com>
2022-10-22 15:46:31 +0530

    task6

--- secondfile.js
+++ secondfile.js
@@ -33,13 +33,49 @@ header.style.borderBottom='solid 3px #000'
 
 //get element by tag//
 
-var li=document.getElementsByTagName('li')
-console.log(li)
-console.log(li)
-// li[1].style.fontweight='bold'
-li[2].style.background='yellow'
+// var li=document.getElementsByTagName('li')
+// console.log(li)
+// console.log(li)
+// // li[1].style.fontweight='bold'
+// li[2].style.background='yellow'
 
-for(let i=0; i<li.length ; i++)
+// for(let i=0; i<li.length ; i++)
+// {
+//     li[i].style.backgroundColor='green'
+// }
+
+
+///queryselector//
+
+// var header=document.querySelector('#main-header')
+// header.style.borderBottom='solid 4px #ccc'
+
+// var input=document.querySelector('input')
+// input.value='enter here'
+
+// var inputt=document.querySelector('input[type="submit"]')
+// inputt.value="send"
+
+// var items=document.querySelector('.list-group-item:nth-child(2)')
+// items.style.backgroundColor="green"
+
+// var items=document.querySelector('.list-group-item:nth-child(3)')
+// items.style.color="transparent"
+
+/////queryselectorall///
+
+var titles=document.querySelectorAll('.title')
+console.log(titles)
+titles[0].textContent='hello'
+
+var odd=document.querySelectorAll('li:nth-child(odd)')
+
+for(let i=0; i<odd.length;i++)
 {
-    li[i].style.backgroundColor='green'
+    odd[i].style.backgroundColor='green'
+}
+var even=document.querySelectorAll('li:nth-child(even)')
+for(let i=0;i<even.length;i++)
+{
+    even[i].style.backgroundColor='yellow'
 }
