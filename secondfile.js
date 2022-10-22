644879ef75b4e5ec07b92a42dc65c592d7465bdb
mohd zubair <mdzubair3782@gmail.com>
2022-10-22 13:09:33 +0530

    task5

--- secondfile.js
+++ secondfile.js
@@ -17,10 +17,29 @@
 var header=document.getElementById('main-header')
 header.style.borderBottom='solid 3px #000'
 
-var items=document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[1])
-// items[1].style.fontweight='bold'
 
-items[2].style.background='green'
-items.style.fontweight='bold'
+// //get element by class//
+// var items=document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[1])
+// // items[1].style.fontweight='bold'
+
+// items[2].style.background='green'
+// // items.style.font_weight='bold'
+//  for(let i=0; i<items.length;i++)
+//  {
+//     items[i].style.backgroundcolor="red"
+//  }
+
+//get element by tag//
+
+var li=document.getElementsByTagName('li')
+console.log(li)
+console.log(li)
+// li[1].style.fontweight='bold'
+li[2].style.background='yellow'
+
+for(let i=0; i<li.length ; i++)
+{
+    li[i].style.backgroundColor='green'
+}
